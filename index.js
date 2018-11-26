b24b70bfe37af76c5e558e89f18e85118c8a323b
Jared Weinberger <jared.j.weinberger@gmail.com>
2018-11-26 12:18:22 -0700

    Converted website to ejs

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
 const express = require('express')
 const logger = require('morgan')
+const expressLayouts = require('express-ejs-layouts')
 const port = 8080
 const server = express();
 
-// best stab at enum for page
-const Home = 0
-const About = 1
-const WebDev = 2
-const MartialArts = 3
-const Games = 4
-const Fetch = 5
-
-let page = Home;
-
-server.set('view-engine', 'ejs')
+const pagePaths = {
+    home: 'home',
+    aboutMe: 'about-me',
+    webDev: 'web-dev',
+    martialArts: 'martial-arts',
+    games: 'games',
+    fetch: 'fetch',
+    form: 'form'
+}
 
 
-server.use( express.static('static') )
+function initServer( server )
+{
+    server.set( 'view engine', 'ejs' )
+    server.use( expressLayouts )
+    server.use( express.static('static') )
 
-server.listen(port)
+    handleRouting( server )
 
+    server.listen( port )
+}
+
+
+function handleRouting( server )
+{
+    server.get( '/', ( request, response ) => renderHome( response ) )
+    server.get( '/index.html', ( request, response ) => renderHome( response ) )
+    server.get( '/about-me.html', (request, response ) => renderAboutMe( response ) )
+    server.get( '/web-dev.html', ( request, response ) => renderWebDev( response ) )
+    server.get( '/martial-arts.html', ( request, response ) => renderMartialArts( response ) )
+    server.get( '/games.html', ( request, response ) => renderGames( response ) )
+    server.get( '/fetch.html', ( request, response ) => renderFetch( response ) )
+    server.get( '/form.html', ( request, response ) => renderForm( response ) )
+}
+
+
+function renderHome( response )
+{
+    response.render( pagePaths.home, { page: 'home' })
+}
+
+function renderAboutMe( response )
+{
+    response.render( pagePaths.aboutMe, { page: 'about-me' })
+}
+
+function renderWebDev( response )
+{
+    response.render( pagePaths.webDev, { page: 'web-dev' })
+}
+
+function renderMartialArts( response )
+{
+    response.render( pagePaths.martialArts, { page: 'martial-arts' })
+}
+
+function renderGames( response )
+{
+    response.render( pagePaths.games, { page: 'games' })
+}
+
+function renderFetch( response )
+{
+    response.render( pagePaths.fetch, { page: 'fetch'})
+}
+
+function renderForm( response )
+{
+    response.render( pagePaths.form, { page: 'form' })
+}
+
+initServer( server )
